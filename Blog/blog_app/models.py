ae50086347e7923985c67b2c3f82afa3780f26bf
Stojan <stojan.v1997@outlook.com>
2020-09-23 09:32:35 +0200

    Created Post model

--- Blog/blog_app/models.py
+++ Blog/blog_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # auto_now_add=True - postavice datum u trenutku kreiranja objekta, ne moze se dodanji izmeniti
+    date_posted = models.DateTimeField(default=timezone.now)
+    # koristimo ForeignKey da bi mapirali Usera sa Postomx
+    # on_delete=CASCADE - obrisanje post kada se obrise i korisnik
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
--- Blog/blog_app/views.py
+++ Blog/blog_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 posts = [
@@ -28,7 +29,7 @@ posts = [
 
 def home(request):
 
-    return render(request,'blog_app/home.html',{'posts' : posts,
+    return render(request,'blog_app/home.html',{'posts' : Post.objects.all(),
     'title' : 'Home'})
 
 
